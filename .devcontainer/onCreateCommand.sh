1b53cea726d355aa64fea0ba0e55c613011f6d83
Tomek Zajac <tomek@mastuazi.com>
2025-01-21 11:56:39 +0100

    Updated on create

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -3,7 +3,4 @@
 poetry config virtualenvs.create true
 poetry install --no-interaction --no-ansi
 
-# Install Arista Collection
-poetry run ansible-galaxy install -r ansible-requirements.yml
-
 poetry run invoke start
