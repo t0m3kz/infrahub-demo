f70fd504cd0ce74c2426bbe2fcb7fe9350cc1aea
Tomek Zajac <tomek@mastuazi.com>
2025-02-04 09:50:56 +0100

    structure cleanup

--- generators/generate_dc.py
+++ generators/generate_dc.py
@@ -11,18 +11,17 @@ class DCTopologyGenerator(TopologyGenerator):
         """Generate topology."""
         self.client.log.setLevel(logging.INFO)
         self.client.log.addHandler(logging.StreamHandler())
-        topology = data["TopologyDataCenter"]["edges"][0]["node"]
-        self.client.log.info(f"Generating topology: {topology['name']['value']}")
-        new_data = clean_data(data)["TopologyDataCenter"][0]
-        # (topology)
+        data =clean_data(data)["TopologyDataCenter"][0]
+        
+        self.client.log.info(f"Generating DC topology: {data['name']}")
         await self._create(
             kind="LocationBuilding",
             data={
-                "name": topology["name"]["value"],
-                "shortname": topology["name"]["value"],
-                "parent": topology["location"]["node"]["id"],
+                "name": data["name"],
+                "shortname": data["name"],
+                "parent": data["location"]["id"],
             },
-            store_key=topology["name"]["value"],
+            store_key=data["name"],
         )
         # create pools
 
@@ -32,7 +31,7 @@ class DCTopologyGenerator(TopologyGenerator):
             name__values=list(
                 set(
                     f"{item['device_type']['manufacturer']['name'].lower()}_{item['role']}"
-                    for item in new_data["design"]["elements"]
+                    for item in data["design"]["elements"]
                 )
             ),
             branch=self.branch,
@@ -40,25 +39,25 @@ class DCTopologyGenerator(TopologyGenerator):
         )
         # create respective IP address pools
         await self._create_ip_pools(
-            new_data["name"],
+            data["name"],
             pools={
-                "management": new_data["management"],
-                "technical": new_data["technical"],
-                "customer": new_data["customer"],
-                "public": new_data["public"],
+                "management": data["management"],
+                "technical": data["technical"],
+                "customer": data["customer"],
+                "public": data["public"],
             },
         )
 
         await self._create_devices(
-            new_data["name"], new_data["design"]["elements"], new_data["id"]
+            data["name"], data["design"]["elements"], data["id"]
         )
-        await self._create_interfaces(new_data["name"], new_data["design"]["elements"])
+        await self._create_interfaces(data["name"], data["design"]["elements"])
         await self._create_oob_connections(
-            new_data["name"], new_data["design"]["elements"], "console"
+            data["name"], data["design"]["elements"], "console"
         )
         await self._create_oob_connections(
-            new_data["name"], new_data["design"]["elements"], "management"
+            data["name"], data["design"]["elements"], "management"
         )
         await self._create_peering_connections(
-            new_data["name"], new_data["design"]["elements"]
+            data["name"], data["design"]["elements"]
         )
