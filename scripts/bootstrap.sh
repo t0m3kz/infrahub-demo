9e21bc01ab4316de955088c6ec5a650a938c94c7
Tomek <107172644+t0m3kz@users.noreply.github.com>
2025-04-16 13:54:05 +0000

    updated queries

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,12 @@
 
 echo "Load schema"
-poetry run infrahubctl schema load model --wait 20
+uv run infrahubctl schema load model --wait 20
 
 echo "Load menu"
-poetry run infrahubctl menu load menu
+uv run infrahubctl menu load menu
 
 echo "Load initial data"
-poetry run infrahubctl run bootstrap/bootstrap.py
+uv run infrahubctl run bootstrap/bootstrap.py
 
 # echo "Add demo repository"
-poetry run infrahubctl repository add DEMO https://github.com/t0m3kz/infrahub-demo.git --ref main --read-only
+uv run infrahubctl repository add DEMO https://github.com/t0m3kz/infrahub-demo.git --ref main --read-only
