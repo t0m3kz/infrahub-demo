bb8f9e5af1f10da13eca634a8696a3a3192213b4
Tomek Zajac <tomek@mastuazi.com>
2025-04-16 19:25:07 +0200

    fixed query group

--- generators/generate_dc.py
+++ generators/generate_dc.py
@@ -69,7 +69,7 @@ class DCTopologyGenerator(TopologyGenerator):
         if data["emulation"]:
             self.client.log.info(f"Assign CLAB group for topology: {data['name']}")
             clab_group = await self.client.get(
-                kind="CoreStandardGroup", name__value="topologies_clab"
+                kind="CoreStandardGroup", name__value="topologies_clab",branch=self.branch
             )
             await clab_group.add_relationships(
                 related_nodes=[data["id"]], relation_to_update="members"
@@ -97,86 +97,3 @@ class DCTopologyGenerator(TopologyGenerator):
         )
 
         await self._create_peering_connections(data["name"], data["design"]["elements"])
-
-        # interfaces = {
-        #     f"{data['name'].lower()}-{item['role']}-{str(j + 1).zfill(2)}": [
-        #         interface["name"]
-        #         for interface in item["template"]["interfaces"]
-        #         if interface["role"] == "console"
-        #     ]
-        #     for item in data["design"]["elements"]
-        #     for j in range(item["quantity"])
-        #     if item["role"] in ["oob", "leaf", "spine", "console"]
-        # }
-
-        # # sort source inerfaces
-        # sources = {
-        #     key: sort_interface_list(value)
-        #     for key, value in interfaces.items()
-        #     if "console" in key
-        # }
-        # destinations = {
-        #     key: sort_interface_list(value)
-        #     for key, value in interfaces.items()
-        #     if key not in sources
-        # }
-
-        # await self.client.filters(kind="InterfacePhysical", device__name__values=list(interfaces.keys()), populate_store=True, parallel=True)
-        # connections = [
-        #     {
-        #         "source": source_device,
-        #         "target": destination_device,
-        #         "source_interface": source_interfaces.pop(0),
-        #         "destination_interface": destination_interfaces.pop(0),
-        #     }
-        #     for source_device, source_interfaces in sources.items()
-        #     for destination_device, destination_interfaces in destinations.items()
-        #     if int(destination_device.split("-")[-1]) % 2
-        #     == int(source_device.split("-")[-1]) % 2
-        # ]
-        # # self.client.log.info(self.store._branches[self.branch].__dict__)
-        # for connection in connections:
-        #     source_endpoint = await self.client.get(
-        #         kind="DcimInterface",
-        #         name__value=connection["source_interface"],
-        #         device__name__value=connection["source"],
-        #     )  # self.store.get_by_hfid((connection["source"], connection["source_interface"]))
-        #     target_endpoint = await self.client.get(
-        #         kind="DcimInterface",
-        #         name__value=connection["destination_interface"],
-        #         device__name__value=connection["target"],
-        #     )  # self.store.get_by_hfid((connection["target"], connection["destination_interface"]))
-        #     source_endpoint.connector = target_endpoint.id
-        #     await source_endpoint.save(allow_upsert=True)
-
-        # self.client.log.info(connections)
-
-        # self.client.log.info(interfaces)
-
-        # create loopback interfaces
-        # await self._create_interfaces(data["name"], data["design"]["elements"])
-        # create Console connections
-        # for item in self.store.get("dc-2-dc_firewall-01_fxp0").interfaces:
-        #     self.client.log.info(item)
-        # self.client.log.info(self.store._branches[self.branch].__dict__)
-        # self.client.log.info(self.store.get("dc-2-dc_firewall-01").interfaces.peers)
-        # self.client.log.info(self.store.get_by_hfid("dc-2-leaf-04").interfaces)
-
-        # self.client.log.info(self.store._branches[self.branch].__dict__)
-        # switch = self.store.get_by_hfid("dc-2-leaf-04").interfaces.peers
-        # for item in switch.interfaces.peers:
-        # for device in devices:
-        #     for interface in device.interfaces:
-        #         self.client.log.info(interface.__dict__)
-        # self.client.log.info(device.interfaces.peers)
-        # self.client.log.info(switch.interfaces.peers.__dict__)
-        # self.client.log.info(switch.interfaces)
-        # self.client.log.info(self.store.get_by_hfid("dc-2-leaf-04").interfaces.__dict__)
-        # self.client.log.info(self.store.get_by_hfid("dc-2-leaf-04").interfaces.__dict__)
-        # test = self.store.get_by_hfid("dc-2-leaf-04")
-
-        # create Management connections
-        # await self._create_oob_connections(
-        #     data["name"], data["design"]["elements"], "management"
-        # )
-        # await self._create_peering_connections(data["name"], data["design"]["elements"])
