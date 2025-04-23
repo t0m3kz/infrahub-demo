7d6b950682e6d3b2d3dfabca174330c5a037b672
Tomek Zajac <tomek@mastuazi.com>
2025-04-23 19:44:13 +0200

    update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ uv run infrahubctl menu load menu --branch $BRANCH
 
 echo "Load initial data"
 # uv run infrahubctl object load data/bootstrap/ --branch $BRANCH
-uv run infrahubctl run .dev/bootstrap/bootstrap.py --branch $BRANCH
+uv run infrahubctl run bootstrap/bootstrap.py --branch $BRANCH
 
-# echo "Add demo repository"
-# uv run infrahubctl repository add DEMO https://github.com/t0m3kz/infrahub-demo.git --ref main --read-only
+echo "Add demo repository"
+uv run infrahubctl repository add DEMO https://github.com/t0m3kz/infrahub-demo.git --ref main --read-only
